052a97f588766a1b547a6041add93dc55661dc97
smartrove <olaokunolalekan@gmail.com>
2023-07-14 06:38:28 +0100

    updates

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const dotenv = require("dotenv/config");
 const morgan = require("morgan");
 const dbConnection = require("./config/dbConnect");
 const url = process.env.URL;
+const userUrl = `${url}/user`;
+const postUrl = `${url}/user`;
+const categoryUrl = `${url}/user`;
+const commentUrl = `${url}/user`;
 const port = process.env.PORT || 6000;
 
 //import routers
@@ -13,17 +17,19 @@ const categoryRouter = require("./routes/categories/categories");
 const errorHandler = require("./middlewares/globalErrorHandler");
 const isAdmin = require("./middlewares/isAdmin");
 const app = express();
+
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(`${url}/user`, userRouter);
-app.use(`${url}/post`, postRouter);
-app.use(`${url}/category`, categoryRouter);
-app.use(`${url}/comment`, commentRouter);
+app.use(userUrl, userRouter);
+app.use(postUrl, postRouter);
+app.use(categoryUrl, categoryRouter);
+app.use(commentUrl, commentRouter);
 
 app.use(errorHandler);
 app.use("*", (req, res) => {
   res.status(404).json({ message: `${req.originalUrl}- is not found` });
 });
 app.use(isAdmin);
+
 
 app.listen(port, () => console.log(`server started at port ${port}`));
